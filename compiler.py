3eac529a7cbaa70f6a2698646b819d85edd7fef6
This is XiaoDeng <1744793737@qq.com>
2023-04-26 21:25:39 +0800

    提交部分没有提交的文件

--- compiler.py
+++ compiler.py
@@ -2,8 +2,13 @@ import lexer
 from rich import print
 import preporcessor
 import parser
+import visitor
+import json
 
 if __name__ == "__main__":
     with open("./helloworld.xr", encoding="utf-8") as f:
-        result = parser.Parser(lexer.parse(preporcessor.prepare(f.read()))).parse()
+        result = visitor.codegen(parser.Parser(lexer.parse(
+            preporcessor.prepare(f.read()))).parse())
     print(result)
+    json.dump(result, open("./helloworld.xr.json", "w", encoding="utf-8"))
+
